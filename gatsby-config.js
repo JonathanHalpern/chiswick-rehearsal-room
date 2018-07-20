9d071e6acf0ad0a86a9dd3916c6d4231dc1d004c
Jonathan Halpern <jonnyhalpern@gmail.com>
2018-07-20 14:21:57 +0800

    added plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,12 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
-    'gatsby-plugin-netlify-cms',
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 };
