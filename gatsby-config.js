cb40abdf0833c1a78a4862c74f1f91b61804888e
Jonathan Halpern <jonnyhalpern@gmail.com>
2018-07-20 14:50:38 +0800

    try live

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,12 +72,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
+    'gatsby-plugin-netlify-cms',
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 };
