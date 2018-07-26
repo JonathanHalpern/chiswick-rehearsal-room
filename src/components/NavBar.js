466e63d46b8de9bd07ed117dcc47931ba3e49982
Jonathan Halpern <jonnyhalpern@gmail.com>
2018-07-26 10:37:58 +0800

    navbar hamburger and folding menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,25 +2,40 @@ import React from 'react';
 import Link from 'gatsby-link';
 import styled from 'styled-components';
 import { compose, withStateHandlers, lifecycle } from 'recompose';
+import Hamburger from './Hamburger';
 
 const withData = lifecycle({
   componentDidMount() {
     window.addEventListener('scroll', this.props.handleScroll);
+    window.addEventListener('resize', this.props.updateDimensions);
   },
 
   componentWillUnmount() {
     window.removeEventListener('scroll', this.props.handleScroll);
+    window.removeEventListener('resize', this.props.updateDimensions);
   },
 });
 
 const handlers = withStateHandlers(
   () => ({
-    scrollTop: 0,
+    scrollTop: document.documentElement.scrollTop,
+    width: window.innerWidth,
+    isMenuOpen: false,
   }),
   {
     handleScroll: () => () => {
       return {
         scrollTop: document.documentElement.scrollTop,
+      };
+    },
+    updateDimensions: () => () => {
+      return {
+        width: window.innerWidth,
+      };
+    },
+    toggleMenu: ({ isMenuOpen }) => () => {
+      return {
+        isMenuOpen: !isMenuOpen,
       };
     },
   },
@@ -31,9 +46,10 @@ const StyledLink = styled(Link)`
   color: white;
   :hover {
     text-decoration: none;
-    color: red;
+    color: grey;
   }
 `;
+
 const Wrapper = styled.div`
   z-index: 2;
   transition: background 0.5s, padding 0.5s;
@@ -43,25 +59,47 @@ const Wrapper = styled.div`
   top: 0;
   width: 100%;
   margin: 0;
+  @media (max-width: 700px) {
+    padding: 0;
+  }
 `;
 
 const StyledList = styled.ul`
   margin: 0
   float: right;
+  display: flex;
+  position: relative;
+  @media (max-width: 700px) {
+    transition: height 0.5s, opacity 0.8s;
+    opacity: ${props => (props.isMenuOpen ? '1' : '0')};
+    height: ${props =>
+      props.isMenuOpen ? `${props.listLength * props.listHeight}px` : '0px'};
+    overflow: hidden;
+    flex-direction: column;
+    float: none;
+  }
 `;
 
 const StyledListItem = styled.li`
-  display: inline-block;
-  margin: 0 25px 0 0;
+  margin: 0;
+  padding: 0 25px 0 0;
+  @media (max-width: 700px) {
+    padding: 10px 20px;
+    background: black;
+  }
   text-transform: uppercase;
 `;
 
 const NavBar = compose(
   handlers,
   withData,
-)(({ pageList, scrollTop }) => (
+)(({ pageList, scrollTop, width, toggleMenu, isMenuOpen }) => (
   <Wrapper scrollTop={scrollTop}>
-    <StyledList>
+    {width <= 700 && <Hamburger isActive={isMenuOpen} onClick={toggleMenu} />}
+    <StyledList
+      isMenuOpen={isMenuOpen}
+      listLength={pageList.length}
+      listHeight={44}>
       {pageList.map(pageItem => (
         <StyledListItem key={pageItem.path}>
           <StyledLink to={pageItem.path}>
--- src/templates/book-page.js
+++ src/templates/book-page.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import BookingContainer from '../components/BookingContainer';
 
-export default ({ data }) =>
-  console.log(data) || (
-    <div>
-      <h1>{data.markdownRemark.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-      {typeof window !== 'undefined' && (
-        <BookingContainer
-          timeSlots={data.markdownRemark.frontmatter.timeSlots}
-        />
-      )}
-    </div>
-  );
+export default ({ data }) => (
+  <div>
+    <h1>{data.markdownRemark.frontmatter.title}</h1>
+    <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+    {typeof window !== 'undefined' && (
+      <BookingContainer timeSlots={data.markdownRemark.frontmatter.timeSlots} />
+    )}
+  </div>
+);
 
 export const bookPageQuery = graphql`
   query BookPage($path: String!) {
